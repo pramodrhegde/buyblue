6634271667498ff2b5e429e2ed4a1ce1509bc8a2
pramodrhegde <pramod.rh@gmail.com>
2017-03-25 13:21:46 +0530

    config changes

--- dist/bundle.js
+++ dist/bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "D:\\Work\\Rentomojo-assignment\\buyblue/dist/";
+/******/ 	__webpack_require__.p = "/assets/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 36);
