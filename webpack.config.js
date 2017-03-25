c37a410910296ffa0044a5c4e56a157826148ce9
pramodrhegde <pramod.rh@gmail.com>
2017-03-25 13:15:32 +0530

    config changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: __dirname + '/dist/',
     filename: 'bundle.js',
-    publicPath: __dirname + '/dist/'
+    publicPath: './dist/'
   },
   module: {
     loaders: [{
